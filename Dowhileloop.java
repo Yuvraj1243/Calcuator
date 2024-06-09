e5ea21818403efecfe1dbebd7796f339e8564f82
Yuvraj1243 <yuvrajprince01@gmail.com>
2024-06-09 06:51:14 +0530

    Create Dowhileloop.java

---
+++ Dowhileloop.java
@@ -0,0 +1,12 @@
+package loopstatements;
+
+public class Dowhileloop {
+    public static void main(String[] args) {
+        int i = 11;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i % 2 == 1);
+        System.out.println("yes i am do while loop");
+    }
+}
