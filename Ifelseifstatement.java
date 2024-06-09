fcbc9ef24da63add574e7564f3a365e777e108a3
Yuvraj1243 <yuvrajprince01@gmail.com>
2024-06-10 00:22:06 +0530

    Create  Ifelseifstatement.java

---
+++ Ifelseifstatement.java
@@ -0,0 +1,12 @@
+ public class NumberCheckExample {
+    public static void main(String[] args) {
+        int number = 300;
+        if (number > 400) {
+            System.out.println("The number is greater than 400");
+        } else if (number == 300) {
+            System.out.println("The number is equal to 300");
+        } else {
+            System.out.println("The number is neither greater than 400 nor equal to 300");
+        }
+    }
+}
