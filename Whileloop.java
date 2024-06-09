655387536d1a55eecdfefcef85dd94e05c909999
Yuvraj1243 <yuvrajprince01@gmail.com>
2024-06-09 06:56:05 +0530

    Create Whileloop.java

---
+++ Whileloop.java
@@ -0,0 +1,14 @@
+package loopstatements;
+
+public class Whileloop {
+	public static void main(String[] args) {
+		int i=35;
+		
+		if(i%7==0) {
+			System.out.println("yes i am while loop");
+			i++;
+		}
+		while(i%7==1);
+	}
+
+}
