270396db88b3b7d6626c8dae8e9fa4bb07f7c492
Yuvraj1243 <yuvrajprince01@gmail.com>
2024-06-10 00:00:50 +0530

    Create PrintElements.java

---
+++ PrintElements.java
@@ -0,0 +1,14 @@
+public class PrintElements{
+    public static void main(String[] args){
+        int arr[] = {1,2,3,4,5,6,7};
+        System.out.println("Original Array: ");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Reverse Array: ");
+        for(int i =arr.length-1;i>=0;i--){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
