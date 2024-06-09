a6f68d33d095623795b87ccfeaa5e7fc8c43461c
Yuvraj1243 <yuvrajprince01@gmail.com>
2024-06-10 00:07:05 +0530

    Create oopsConcept.java

---
+++ oopsConcept.java
@@ -0,0 +1,12 @@
+package oopsConcept;
+
+public class PersonMain {
+	public static void main(String[] args) {
+		
+	
+	EncapsulatePerson ec=new EncapsulatePerson("raju",27);
+	
+		
+	System.out.println(ec);
+	}
+}
