d4e3ca59d379c8627048eefad0b4182917de53bc
Yuvraj1243 <yuvrajprince01@gmail.com>
2024-06-09 06:54:05 +0530

    Create ForEachLoop.java

---
+++ ForEachLoop.java
@@ -0,0 +1,13 @@
+package loopstatements;
+
+public class ForEachLoop {
+	public static void main(String[] args) {
+		int[] a= {1,2,3,4,5,6,6};
+		for(int j:a) {
+			System.out.println(j);
+			
+		}
+			
+		}
+	}
+
