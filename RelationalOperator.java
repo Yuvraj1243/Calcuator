51cdd8ed5cbfafb0c8fefbe3e848fb3b8bdf6b86
Yuvraj1243 <yuvrajprince01@gmail.com>
2024-06-10 00:04:37 +0530

    Create RelationalOperator.java

---
+++ RelationalOperator.java
@@ -0,0 +1,14 @@
+import java.io.*;
+class RelationalOperator {
+    public static void main(String[] args) {
+        int a=43;
+        int b=54;
+        int c=35;
+       System.out.println(a<b);//true
+       System.out.println(a>b);//false
+       System.out.println(a<=b);//true
+       System.out.println(a>=b);//false
+       System.out.println(a==b);//false
+       System.out.println(a!=b);//true
+    }
+}
